e566aa2a6fff046effe6fb75f76514d7a6b31ae1
hxr1z <06_postfix.feeds@icloud.com>
2026-01-18 16:06:05 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+const dbConfig= {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false },
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Server is running! Try /recyclables to see the data.');
+});
+
+app.get('/recyclables', async (req,res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.recyclables');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for recyclables'});
+    }
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
